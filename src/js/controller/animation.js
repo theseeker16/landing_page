8e8bdf32ef36761270c4663160ddee735ac332aa
Josue Guerrero <josue.guerrero@interaction.cr>
2019-04-24 14:29:29 -0600

    final landing-page

--- src/js/controller/animation.js
+++ src/js/controller/animation.js
@@ -1,10 +1,12 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   console.log("ready");
-  AOS.init();
+  AOS.init({
+    once: true
+  });
 
-  $(".hero__scroll").click(function() {
+  $(".hero__scroll").click(function () {
     $('html, body').animate({
-        scrollTop: $(".softek").offset().top
+      scrollTop: $(".softek").offset().top
     }, 2500);
-});
+  });
 });
